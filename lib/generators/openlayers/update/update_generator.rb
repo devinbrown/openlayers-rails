3b28a073b7404bd631fa8e7a5e69b7ccc5494c23
Devin Brown <devin.g.brown@gmail.com>
2013-03-10 21:32:46 -0600

    refactor in progress, working "checking in"

--- lib/generators/openlayers/update/update_generator.rb
+++ lib/generators/openlayers/update/update_generator.rb
@@ -1,3 +1,10 @@
+# # TODO: extract module
+# module Thor::Actions
+#   def source_paths
+#     [File.join(Rails.root, "openlayers")]
+#   end
+# end
+
 module Openlayers
   module Generators
     class UpdateGenerator < Rails::Generators::Base
@@ -5,27 +12,35 @@ module Openlayers
       source_root File.expand_path('../templates', __FILE__)
       desc "This updates the local OpenLayers library"
 
-      def update
-        current_directory = Rails.root
-        source_directory  = File.join current_directory, 'openlayers'
-        assets_directory  = File.join current_directory, 'vendor', 'assets'
-        docs_directory    = File.join current_directory, 'doc'
+      CURRENT_DIRECTORY = Rails.root
 
+      # source_path() ?
+      SOURCE_DIRECTORY   = File.join CURRENT_DIRECTORY, 'openlayers'
+
+      # destination_root() ?
+      ASSETS_DIRECTORY    = File.join CURRENT_DIRECTORY, 'vendor', 'assets'
+      DOCS_DIRECTORY      = File.join CURRENT_DIRECTORY, 'doc'
+
+      def prepare_directories
+        FileUtils.rm_rf File.join Rails.root, "openlayers"
+      end
+
+      def update_openlayers_submodule
         puts "Updating sources..."
-
-        # git submodule taps into the most recent OpenLayer version via git repo
         `git submodule update`
+      end
 
-        puts "Trashing the old stuff..."
+      def trash_vendored_assets
+        # FIXME: this will destroy ALL vendored assets, not sure if this is acutally needed
+        # puts "Trashing the old stuff..."
+        # FileUtils.rm_r Dir.glob(File.join ASSETS_DIRECTORY, '*')
+      end
 
-        # FIXME: this will destroy all vendored assets
-        FileUtils.rm_r Dir.glob(File.join assets_directory, '*')
-
+      def build_openlayers
         puts "Building the main JS file..."
-
-        build_dir         = File.join source_directory, 'build'
+        build_dir = File.join SOURCE_DIRECTORY, 'build'
         # NOTE: will need to make this final name specified by the user
-        build_destination = File.join assets_directory, 'javascripts', 'openlayers', 'OpenLayers.js'
+        build_destination = File.join ASSETS_DIRECTORY, 'javascripts', 'openlayers', 'OpenLayers.js'
 
         # NOTE: looks like we've found a way to build this mofo!
         # mkdir_p = final dest. + all parents
@@ -33,30 +48,35 @@ module Openlayers
         # FIXME: "python: can't open file './buildUncompressed.py': [Errno 2] No such file or directory"
         FileUtils.mkdir_p File.dirname build_destination
         `cd #{build_dir} && python ./buildUncompressed.py full #{build_destination}`
+      end
 
-        puts "Copying over other files..."
+      def copy_extra_files
+        # license and authors
+        copy_file File.join(SOURCE_DIRECTORY, "authors.txt"), File.join(CURRENT_DIRECTORY, "authors.txt")
+        copy_file File.join(SOURCE_DIRECTORY, "LICENSE.txt"), File.join(CURRENT_DIRECTORY, "LICENSE.txt")
+      end
 
-        # FIXME: "No such file or directory - /Users/dgb/Dropbox/development/openlayers-rails/openlayers/authors.txt"
-        # FIXME: "No such file or directory - /Users/dgb/Dropbox/development/openlayers-rails/openlayers/license.txt"
-        # copy author and lisence from (updated) source_directory to deployment directory (current_directory, based of gem)
-        # ["authors.txt", "license.txt"].each do |file|
-        #   FileUtils.cp File.join(source_directory, file), File.join(current_directory, file)
-        # end
-
-        # appears to be copying relevant files from "openlayers" to vendor/assets
-        # source => destination
-        {
-          "img/*"               => File.join(assets_directory, "images/openlayers/img"),
-          "theme/default/img/*" => File.join(assets_directory, "images/openlayers/theme/default/img"),
-          "theme/default/*.css" => File.join(assets_directory, "stylesheets/openlayers/theme/default"),
-          "licenses/*"          => File.join(docs_directory, "licenses")
-        }.each do |source, destination|
-          FileUtils.mkdir_p destination
-          FileUtils.cp_r Dir.glob(File.join source_directory, source), destination
+      def copy_images
+        Dir.glob(File.join(SOURCE_DIRECTORY, "img", "*")).each do |source_file|
+          copy_file source_file, File.join(ASSETS_DIRECTORY, "images/openlayers/img/#{source_file.split("/").last}")
         end
+      end
 
-        # TODO: need to clean up files that were used for precompiling, especially if running rake task from gem'd loc
-        puts "Done! Now update version information and release!"
+      def copy_default_images
+        Dir.glob(File.join(SOURCE_DIRECTORY, "theme", "default", "img", "*")).each do |source_file|
+          copy_file source_file, File.join(ASSETS_DIRECTORY, "images/openlayers/theme/default/img/#{source_file.split("/").last}")
+        end
+      end
+
+      def copy_stylesheets
+        Dir.glob(File.join(SOURCE_DIRECTORY, "theme", "default", "*.css")).each do |source_file|
+          copy_file source_file, File.join(ASSETS_DIRECTORY, "stylesheets/openlayers/theme/default/#{source_file.split("/").last}")
+        end
+      end
+
+      # Remove files used in build process
+      def cleanup
+        FileUtils.rm_rf File.join Rails.root, "openlayers"
       end
     end
   end
