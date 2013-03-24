f7b920b03434ffe973c658fb79b743cef8bd8132
Devin Brown <devin.g.brown@gmail.com>
2013-03-24 16:01:03 -0600

    remove rails app from gem

--- app/assets/javascripts/openlayers-rails.js
+++
@@ -1,4 +0,0 @@
-//= require openlayers/OpenLayers
-
-// configured location for openlayers.js (?)
-OpenLayers._getScriptLocation = function(){ return '/assets/openlayers/'; };
--- lib/generators/openlayers/update/update_generator.rb
+++ lib/generators/openlayers/update/update_generator.rb
@@ -17,14 +17,17 @@ module Openlayers
       ASSETS_DIRECTORY    = File.join CURRENT_DIRECTORY, 'vendor', 'assets'
       DOCS_DIRECTORY      = File.join CURRENT_DIRECTORY, 'doc'
 
+      # clear old cruft, prepare for updated openlayers files
       def prepare_directories
         FileUtils.rm_rf File.join Rails.root, "openlayers"
       end
 
+      # get the updated openlayers code
       def update_openlayers_submodule
         `git submodule update`
       end
 
+      # place primary OpenLayer JS into 'vendor/assets'
       def build_openlayers
         build_dir = File.join SOURCE_DIRECTORY, 'build'
 
@@ -35,31 +38,35 @@ module Openlayers
         `cd #{build_dir} && python ./buildUncompressed.py full #{build_destination}`
       end
 
-      def copy_extra_files
-        # license and authors
-        copy_file File.join(SOURCE_DIRECTORY, "authors.txt"), File.join(CURRENT_DIRECTORY, "authors.txt")
-        copy_file File.join(SOURCE_DIRECTORY, "LICENSE.txt"), File.join(CURRENT_DIRECTORY, "LICENSE.txt")
-      end
+      # TODO: generate if specified from generate command
+      # def copy_extra_files
+      #   # license and authors
+      #   copy_file File.join(SOURCE_DIRECTORY, "authors.txt"), File.join(CURRENT_DIRECTORY, "authors.txt")
+      #   copy_file File.join(SOURCE_DIRECTORY, "LICENSE.txt"), File.join(CURRENT_DIRECTORY, "LICENSE.txt")
+      # end
 
+      # place primary OpenLayer images into 'vendor/assets'
       def copy_images
         Dir.glob(File.join(SOURCE_DIRECTORY, "img", "*")).each do |source_file|
           copy_file source_file, File.join(ASSETS_DIRECTORY, "images/openlayers/img/#{source_file.split("/").last}")
         end
       end
 
+      # place default OpenLayer images into 'vendor/assets'
       def copy_default_images
         Dir.glob(File.join(SOURCE_DIRECTORY, "theme", "default", "img", "*")).each do |source_file|
           copy_file source_file, File.join(ASSETS_DIRECTORY, "images/openlayers/theme/default/img/#{source_file.split("/").last}")
         end
       end
 
+      # place OpenLayer stylesheets into 'vendor/assets'
       def copy_stylesheets
         Dir.glob(File.join(SOURCE_DIRECTORY, "theme", "default", "*.css")).each do |source_file|
           copy_file source_file, File.join(ASSETS_DIRECTORY, "stylesheets/openlayers/theme/default/#{source_file.split("/").last}")
         end
       end
 
-      # Remove files used in build process
+      # Remove files used in build process (e.g. updated / downloaded openlayers files)
       def cleanup
         FileUtils.rm_rf File.join Rails.root, "openlayers/*"
       end
