060c098759212d6904a8c7d97e9e022e26f10714
Devin Brown <devin.g.brown@gmail.com>
2013-03-30 16:39:20 -0600

    copy images to proper location, shorten JS copy

--- lib/generators/openlayers/install/install_generator.rb
+++ lib/generators/openlayers/install/install_generator.rb
@@ -1,23 +1,25 @@
-# FIXME: refactor / simplify / make more Thorsy
 module Openlayers
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc "Install the most recent version of OpenLayers to the Asset Pipeline"
 
+      # TODO: test this as repo'd gem (not locally installed)
       source_root File.expand_path("../../../../../vendor/assets", __FILE__)
 
-      JS_PATH = File.join source_root, "javascripts"
-      CSS_PATH = File.join source_root, "stylesheets"
-      IMG_PATH = File.join source_root, "images"
-
       def add_javascript
-        copy_file "javascripts/Openlayers.js", "vendor/assets/javascripts/OpenLayers.js"
+        js_dest = "vendor/assets/javascripts"
+        copy_file "javascripts/Openlayers.js", "#{js_dest}/OpenLayers.js"
       end
 
       def add_stylesheets
       end
 
       def add_images
+        img_dest = "vendor/assets/images/img"
+        img_source = File.join source_paths, "images", "img"
+        Dir.glob("#{img_source}/*").each do |f|
+          copy_file "images/img/#{File.basename f}", "#{img_dest}/#{f}"
+        end
       end
     end
   end
